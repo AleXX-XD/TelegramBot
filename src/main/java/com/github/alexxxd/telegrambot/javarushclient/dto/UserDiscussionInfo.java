cc0d90f171e07ffe6392cedfb34f759c2ce1cd03
AleXX-XD <le7v7el@yandex.ru>
2022-05-25 15:06:37 +0500

    STEP_5_TB-1: Add repository layer

---
+++ src/main/java/com/github/alexxxd/telegrambot/javarushclient/dto/UserDiscussionInfo.java
@@ -0,0 +1,11 @@
+package com.github.alexxxd.telegrambot.javarushclient.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDiscussionInfo
+{
+    private Boolean isBookmarked;
+    private Integer lastTime;
+    private Integer newCommentsCount;
+}
